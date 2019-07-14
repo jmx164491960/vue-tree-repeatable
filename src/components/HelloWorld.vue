b241be6de163bab612629b9ede5b94dba362b363
jesseJiang2019 <164491960@qq.com>
2019-07-14 12:34:54 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <el-tree
+      :data="data"
+      show-checkbox
+      node-key="id"
+    >
+    </el-tree>
+  </section>
+</template>
+
+<script>
+import ElTree from './tree/tree';
+export default {
+  name: 'HelloWorld',
+  components: {
+    ElTree
+  },
+  data () {
+    return {
+      data: [
+        {
+          id: 'p1',
+          label: 'parent1',
+          children: [
+            {
+              id: 'c1',
+              label: 'child2'
+            }
+          ]
+        },
+        {
+          id: 'p2',
+          label: 'parent2',
+          children: [
+            {
+              id: 'c2',
+              label: 'child2'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
